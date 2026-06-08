e893c1db54e38bb8cd2d758475d331673b2755b3
darilmaz-cmd <darilmazfatihosman@gmail.com>
2026-06-08 20:22:04 +0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "REIGN Bot Aktif!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 import discord
 from discord.ext import commands, tasks
 from discord import app_commands
@@ -115,4 +131,5 @@ async def uyum(interaction: discord.Interaction, hedef_kullanici: discord.Member
         await interaction.followup.send("Sistemsel bir anomali oluştu. Lütfen botun konsoluna (terminal) bak.")
 
 if __name__ == "__main__":
+    keep_alive()  # Botu çalıştırmadan önce web sunucusunu aç
     bot.run(DISCORD_TOKEN)
